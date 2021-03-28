6cf6a70c080c9adbd92fdee29170f2e9e1076f1a
chadcomiter <chadcomiter@gmail.com>
2021-03-28 18:01:52 -0400

    Added Card And Search Bar

--- src/components/weather-display.js
+++ src/components/weather-display.js
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import {connect} from 'react-redux';
 import weatherService from '../services/weather-service.js';
 
@@ -8,14 +8,64 @@ const WeatherDisplay = (
         findWeatherForCity
     }
 ) => {
-
+    const [detailed, setDetailed] = useState(false)
     useEffect(() => {
         findWeatherForCity()
     }, [])
 
     return(
         <>
-        <p>{JSON.stringify(weatherForCity)}</p>
+        <input 
+        type="text" 
+        placeholder="Search"
+        ></input>
+        {
+            !detailed && 
+            <>
+                <div className="col-xl-3 col-lg-4 col-sm-6 col-xs-12">
+                        <div className="card">
+                            <div className="card-body"> 
+                                <h5 className="card-title">
+                                    {weatherForCity.name}
+                                </h5>
+                                <i onClick={() => {
+                                    setDetailed(true)
+                                    console.log("Clicked")
+                                }}
+                                className="fas fa-plus fa-2x float-right"></i>
+                                <img src="https://images.unsplash.com/photo-1532124957326-34c5605398?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1567&q=80"
+                                    className="card-img-top" alt="..."/>
+                            </div>
+                        </div>
+                    </div>
+            </>
+        }
+        {
+            detailed &&
+            <>
+                <div className="col-xl-3 col-lg-4 col-sm-6 col-xs-12">
+                    <div className="card">
+                        <div className="card-body"> 
+                            <h5 className="card-title">
+                                {weatherForCity.name}
+                            </h5>
+                            <i onClick={() => {
+                                setDetailed(false)
+                            }}
+                            className="fas fa-minus fa-2x float-right"></i>
+                            <img src="https://images.unsplash.com/photo-1532124957326-34c5605398?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1567&q=80"
+                                className="card-img-top" alt="..."/>
+                            <ul>
+                                <li><p>Temperature: {weatherForCity.main.temp} Kelvin</p></li>
+                                <li><p>Feels Like: {weatherForCity.main.feels_like} Kelvin</p></li>
+                                <li><p>Low: {weatherForCity.main.temp_min} Kelvin</p></li>
+                                <li><p>Hight: {weatherForCity.main.temp_max} Kelvin</p></li>
+                            </ul>
+                         </div>
+                    </div>
+                </div>
+            </>
+        }
         </>
     )
 }
